2b39416e29f77b40fbb98cf26673bbbc26aaff70
flatironschool <danliu277@gmail.com>
2020-08-18 17:33:34 -0400

    Create game model

--- src/games/models.py
+++ src/games/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+class Game(models.Model):
+    name = models.CharField(max_length=120)
+    genre = models.CharField(max_length=120)
+    sales_price = models.DecimalField(decimal_places=2)
+    vendor_cost = models.DecimalField(decimal_places=2)
+    threshold = models.DecimalField(decimal_places=2)
+
+    def __str__(self):
+        return self.name
