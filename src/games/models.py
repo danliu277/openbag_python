92d6f299299631529801c8a6c01a5fab3b4a8eaf
flatironschool <danliu277@gmail.com>
2020-08-18 17:35:39 -0400

    Create migration for game

--- src/games/models.py
+++ src/games/models.py
@@ -3,9 +3,9 @@ from django.db import models
 class Game(models.Model):
     name = models.CharField(max_length=120)
     genre = models.CharField(max_length=120)
-    sales_price = models.DecimalField(decimal_places=2)
-    vendor_cost = models.DecimalField(decimal_places=2)
-    threshold = models.DecimalField(decimal_places=2)
+    sales_price = models.DecimalField(max_digits=5, decimal_places=2)
+    vendor_cost = models.DecimalField(max_digits=5, decimal_places=2)
+    threshold = models.DecimalField(max_digits=5, decimal_places=2)
 
     def __str__(self):
         return self.name
