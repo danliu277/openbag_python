403d1088f2b535e3227569f105e0c5789c46d87b
flatironschool <danliu277@gmail.com>
2020-08-18 17:22:57 -0400

    Add employee route

--- src/openbag_python/urls.py
+++ src/openbag_python/urls.py
@@ -4,5 +4,6 @@ from django.urls import path, include
 urlpatterns = [
     path('api-auth/', include('rest_framework.urls')),
     path('admin/', admin.site.urls),
-    path('customer/', include('customers.api.urls'))
+    path('customer/', include('customers.api.urls')),
+    path('employee/', include('employees.api.urls')),
 ]
