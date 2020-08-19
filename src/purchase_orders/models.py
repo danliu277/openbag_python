a84a7878760bd78024dc8da6ebd31daefa4322ef
flatironschool <danliu277@gmail.com>
2020-08-19 17:05:33 -0400

    Fix purchaseOrder model

--- src/purchase_orders/models.py
+++ src/purchase_orders/models.py
@@ -1,10 +1,10 @@
 from django.db import models
 
 class PurchaseOrder(models.Model):
-    game_id = stock = models.IntegerField()
-    employee_id = stock = models.IntegerField()
-    vendor_id = stock = models.IntegerField()
-    quantity = stock = models.IntegerField()
+    game_id = models.IntegerField()
+    employee_id = models.IntegerField()
+    vendor_id = models.IntegerField()
+    quantity = models.IntegerField()
 
     def __str__(self):
         return self.name
