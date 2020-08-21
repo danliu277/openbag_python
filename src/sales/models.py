ef88850d026b04b1acb85b0eacc8a29837259a37
flatironschool <danliu277@gmail.com>
2020-08-21 17:25:14 -0400

    Create sales model

--- src/sales/models.py
+++ src/sales/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+class Sales(models.Model):
+    customer_id = models.IntegerField()
+    game_id = models.IntegerField()
+    employee_id = models.IntegerField()
+    quantity = models.IntegerField()
+
+    def __str__(self):
+        return self.name
