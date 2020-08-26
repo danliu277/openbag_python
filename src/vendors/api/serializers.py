aef1596709042f66a93883d67114b5b08f8f504f
flatironschool <danliu277@gmail.com>
2020-08-26 16:49:18 -0400

    Fix vendor serializer

--- src/vendors/api/serializers.py
+++ src/vendors/api/serializers.py
@@ -1,8 +1,8 @@
 from rest_framework import serializers
 
-from sales.models import Sale
+from vendors.models import Vendor
 
 class VendorSerializer(serializers.ModelSerializer):
     class Meta:
-        model = Sale
+        model = Vendor
         fields = ('id', 'email', 'name', 'address')
