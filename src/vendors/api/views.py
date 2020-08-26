7f274feb2e6512ffc4ec1c3536ed2b620e4052ca
flatironschool <danliu277@gmail.com>
2020-08-26 16:46:00 -0400

    Create VendorViewSet

---
+++ src/vendors/api/views.py
@@ -0,0 +1,8 @@
+from sales.models import Vendor
+from .serializers import VendorSerializer
+
+from rest_framework import viewsets
+
+class VendorViewSet(viewsets.ModelViewSet):
+    serializer_class = VendorSerializer
+    queryset = Vendor.objects.all()
